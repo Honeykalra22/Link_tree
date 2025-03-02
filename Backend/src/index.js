b905d278a68c71d5e25a65eff8f06ac8affbe6fb
Hitesh Kalra <hiteshkalra229489@gmail.com>
2025-03-02 15:25:44 +0530

    Refactor database connection handling in index.js and update Vercel configuration paths

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,28 @@
+// import { app } from "./app.js";
+// import { connectDB } from "./db/index.js";
+
+// connectDB()
+//     .then(() => {
+//         const port = process.env.PORT || 5000
+//         app.listen(port, () => {
+//             console.log(`Server is running at port: ${port}`)
+//         })
+//     })
+//     .catch ((err) => (
+//         console.log(`An error while connecting the db ${err}`)
+//     ))
+
+
+
 import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 
-connectDB()
-    .then(() => {
-        const port = process.env.PORT || 5000
-        app.listen(port, () => {
-            console.log(`Server is running at port: ${port}`)
-        })
-    })
-    .catch ((err) => (
-        console.log(`An error while connecting the db ${err}`)
-    ))
+export default async function handler(req, res) {
+    try {
+        await connectDB();
+        return app(req, res);
+    } catch (err) {
+        console.error("Database connection error:", err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}

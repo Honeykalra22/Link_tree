cc1037480b29f395be6255748b2b1b7cf8e12f32
Hitesh Kalra <hiteshkalra229489@gmail.com>
2025-03-02 15:09:33 +0530

    Add .gitignore for Vercel; update app.js for server health check and modify Vercel configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,10 +13,11 @@ app.use(express.json({limit: '128kb'}))
 app.use(express.urlencoded({limit: "128kb"}))
 // app.use(bodyParser)
 
-app.use((req, res, next) => {
-    console.log('Time:', Date.now())
-    next()
-})
+app.get('/', (req, res) => {
+    console.log('Time:', Date.now());
+    res.send('Server is running');
+});
+
 
 import userRouter from './route/user.route.js'
 app.use('/api/v2/user', userRouter)
